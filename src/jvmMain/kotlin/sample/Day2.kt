5a38036998334e359ad4d5d11304d88306950d34
Felix de Souza <fdesouza@palantir.com>
2019-12-12 03:12:51 +0000

    Day 2 part 2

--- src/jvmMain/kotlin/sample/Day2.kt
+++ src/jvmMain/kotlin/sample/Day2.kt
@@ -1,8 +1,9 @@
 package sample
 
 import openFile
-import sample.Day2.part1
+import sample.Day2.part2
 
+data class NounVerb(val noun: Int, val verb: Int)
 object Day2 {
 
     val input = parseInput(openFile("sample/day2.txt")).toMutableList()
@@ -10,7 +11,22 @@ object Day2 {
     fun parseInput(rawInput: String) = rawInput.split(",").map(String::toInt)
 
     fun part1(program: List<Int>): Int {
+        return runProgram(program, NounVerb(12, 2))
+    }
+
+    fun part2(program: List<Int>) {
+        val noun = (1969 - 145) / 48
+        val verb = 92
+        val parameters = NounVerb(noun, verb)
+        println(runProgram(program, parameters))
+        println(parameters)
+    }
+
+    private fun runProgram(program: List<Int>, parameters: NounVerb): Int {
         val mutable = program.toMutableList()
+        mutable[1] = parameters.noun
+        mutable[2] = parameters.verb
+
         var index = 0
         loop@ while (true) {
             val opcode = mutable[index]
@@ -19,13 +35,13 @@ object Day2 {
                 1 -> mutate(mutable, index) { a, b -> a + b }
                 2 -> mutate(mutable, index) { a, b -> a * b }
                 99 -> {
-                    println(mutable)
+//                    println(mutable)
                     break@loop
                 }
                 else -> throw UnsupportedOperationException("unexpected opcode: $opcode")
             }
             index += 4
-            println(mutable)
+//            println(mutable)
         }
 
         return mutable[0]
@@ -39,9 +55,7 @@ object Day2 {
 }
 
 fun main() {
-    val mutableInput = Day2.input.toMutableList()
-    mutableInput[1] = 12
-    mutableInput[2] = 2
-    println("${part1(mutableInput)}")
+//    println("${part1(Day2.input)}")
+    part2(Day2.input)
 }
 
