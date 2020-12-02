f030bf08cf75f0731b248eacef7062f118901c58
Felix de Souza <fdesouza@palantir.com>
2020-12-02 00:08:35 +0000

    Day 1 part 2

--- aoc-2020/src/main/kotlin/aoc2020/Day1.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day1.kt
@@ -3,8 +3,19 @@ package aoc2020
 import common.readLines
 
 object Day1 {
-    val input = readLines("/aoc2020/day1.txt").map { it.toInt() }.toSet()
-    fun part1(numbers: Set<Int>): Int {
-        return numbers.find { (2020 - it) in numbers }!!.let { it * (2020 - it) }
+    val input = readLines("/aoc2020/day1.txt").map { it.toLong() }.toSet()
+    fun part1(numbers: Set<Long>): Long {
+        return doSomething(numbers, 2020)!!
+    }
+
+    private fun doSomething(numbers: Set<Long>, target: Long): Long? {
+        return numbers.find { (target - it) in numbers }?.let { it * (target - it) }
+    }
+
+    fun part2(numbers: Set<Long>): Long {
+        return numbers.associateWith { first -> doSomething(numbers, 2020 - first)?.let { it * first } }
+            .filterValues { it != null }
+            .mapValues { (_, i) -> i!! }
+            .values.first()
     }
 }
--- aoc-2020/src/test/kotlin/aoc2020/Day1KtTest.kt
+++ aoc-2020/src/test/kotlin/aoc2020/Day1KtTest.kt
@@ -4,13 +4,27 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class Day1KtTest {
+    companion object {
+        val testInput = setOf(1721, 979, 366, 299, 675, 1456L)
+    }
+
     @Test
-    internal fun testInput() {
-        assertEquals(514579, Day1.part1(setOf(1721, 979, 366, 299, 675, 1456)))
+    internal fun part1_testInput() {
+        assertEquals(514579, Day1.part1(testInput))
     }
 
     @Test
     internal fun part1() {
         assertEquals(858496, Day1.part1(Day1.input))
+    }
+
+    @Test
+    internal fun part2_testInput() {
+        assertEquals(241861950, Day1.part2(testInput))
+    }
+
+    @Test
+    internal fun part2() {
+        assertEquals(263819430, Day1.part2(Day1.input))
     }
 }
