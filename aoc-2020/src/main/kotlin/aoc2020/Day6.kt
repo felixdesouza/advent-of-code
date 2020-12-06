21d3382e6f613f4a84bd52b0fb8df37353afaa5e
Felix de Souza <fdesouza@palantir.com>
2020-12-06 10:09:47 +0000

    Day 6 part 2

--- aoc-2020/src/main/kotlin/aoc2020/Day6.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day6.kt
@@ -10,12 +10,24 @@ object Day6 {
         return boardingGroups.map { processBoardingGroup(it) }.sum()
     }
 
+    fun part2(boardingGroups: List<String>): Int {
+        return boardingGroups.map { processBoardingGroupPart2(it) }.sum()
+    }
+
     fun parseInput(raw: String): List<String> {
         return raw.split("\n\n")
     }
 
     private fun processBoardingGroup(boardingGroup: String): Int {
         return boardingGroup.replace("\n", "").groupBy { it }
+            .count()
+    }
+
+    private fun processBoardingGroupPart2(boardingGroup: String): Int {
+        val numberOfPassengers = boardingGroup.lines().size
+        return boardingGroup.replace("\n", "").groupingBy { it }
+            .eachCount()
+            .filter { (_, count) -> count == numberOfPassengers }
             .count()
     }
 }
--- aoc-2020/src/test/kotlin/aoc2020/Day6Test.kt
+++ aoc-2020/src/test/kotlin/aoc2020/Day6Test.kt
@@ -23,12 +23,22 @@ internal class Day6Test {
     """.trimIndent()
 
     @Test
-    internal fun name() {
+    internal fun `part 1 test input`() {
         assertEquals(11, Day6.part1(Day6.parseInput(testInput)))
     }
 
     @Test
     internal fun `part 1 real input`() {
-        println(Day6.part1(Day6.parseInput(Day6.input)))
+        assertEquals(6585, Day6.part1(Day6.parseInput(Day6.input)))
+    }
+
+    @Test
+    internal fun `part 2 test input`() {
+        assertEquals(6, Day6.part2(Day6.parseInput(testInput)))
+    }
+
+    @Test
+    internal fun `part 2 real input`() {
+        assertEquals(3276, Day6.part2(Day6.parseInput(Day6.input)))
     }
 }
