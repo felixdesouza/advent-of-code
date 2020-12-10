ffbf99193dcc1524b990316b620a52e8c961936c
Felix de Souza <fdesouza@palantir.com>
2020-12-10 05:40:57 +0000

    Day 10 part 2

--- aoc-2020/src/main/kotlin/aoc2020/Day10.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day10.kt
@@ -6,6 +6,21 @@ object Day10 {
 
     val input = readLines("/aoc2020/day10.txt").map { it.toInt() }
 
+    val testInput2 = """
+        16
+        10
+        15
+        5
+        1
+        11
+        7
+        19
+        6
+        12
+        4
+    """.trimIndent()
+        .lines().map { it.toInt() }
+
     val testInput = """
         28
         33
@@ -57,8 +72,37 @@ object Day10 {
         println(counts)
         return counts.getOrDefault(1, 0).toLong() * counts.getOrDefault(3, 0)
     }
+
+    fun part2(joltages: List<Int>): Long {
+        val sorted = joltages.sorted()
+        val adapterRating = sorted.max()!! + 3
+        val chain = listOf(0).plus(sorted).plus(adapterRating)
+
+        val map = mutableMapOf<Int, Long>()
+
+        fun f(index: Int): Long {
+            if (map.containsKey(index)) {
+                return map[index]!!
+            }
+
+            if (index == chain.size - 1) {
+                return 1
+            }
+
+            val result = chain.withIndex().toList().slice((index + 1 until chain.size))
+                .filter { it.value <= chain[index] + 3 }
+                .map { f(it.index) }
+                .sum()
+
+            map[index] = result
+            return result
+        }
+
+        return f(0)
+    }
 }
 
 fun main() {
     println(Day10.part1(Day10.input))
+    println(Day10.part2(Day10.input))
 }
