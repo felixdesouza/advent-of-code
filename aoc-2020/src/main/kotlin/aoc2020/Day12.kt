603a61774e6aee954906eebdac90c48012f14c89
Felix de Souza <fdesouza@palantir.com>
2020-12-12 05:59:40 +0000

    Day 12 part2

--- aoc-2020/src/main/kotlin/aoc2020/Day12.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day12.kt
@@ -16,22 +16,26 @@ object Day12 {
 
     data class State(val coordinate: Coordinate, val direction: Int)
 
-    fun part1(instructions: List<Instruction>): Int {
-        fun directionToCoord(int: Int): Coordinate {
-            val check = if (int < 0) {
-                (int + 360) % 360
-            } else {
-                int % 360
-            }
-            return when (check) {
-                0 -> Coordinate(0, -1)
-                90 -> Coordinate(1, 0)
-                180 -> Coordinate(0, 1)
-                270 -> Coordinate(-1, 0)
-                else -> throw AssertionError("unexpected $int $check")
-            }
+    data class WaypointState(val shipCoordinate: Coordinate, val waypointVector: Coordinate)
+
+    fun directionToCoord(int: Int): Coordinate {
+        val check = if (int < 0) {
+            (int + 360) % 360
+        } else {
+            int % 360
         }
-        val (newCoord, newDirection) = instructions.fold(State(Coordinate.origin, 90)) { currState, next ->
+        return when (check) {
+            0 -> Coordinate(0, -1)
+            90 -> Coordinate(1, 0)
+            180 -> Coordinate(0, 1)
+            270 -> Coordinate(-1, 0)
+            else -> throw AssertionError("unexpected $int $check")
+        }
+    }
+
+    fun part1(instructions: List<Instruction>): Int {
+
+        val (newCoord, _) = instructions.fold(State(Coordinate.origin, 90)) { currState, next ->
             val (coord, direction) = currState
             val (action, operand) = next
             when (action) {
@@ -46,9 +50,48 @@ object Day12 {
             }
         }
 
-        println(newCoord)
-        println(newDirection)
+        return abs(newCoord.x) + abs(newCoord.y)
+    }
 
+    fun part2(instructions: List<Instruction>): Int {
+        fun rotate(coordinate: Coordinate, int: Int): Coordinate {
+            val check = if (int < 0) {
+                (int + 360) % 360
+            } else {
+                int % 360
+            }
+
+            var (x, y) = coordinate
+
+            val times = check / 90
+
+            for (i in (1..times)) {
+                val temp = x
+                x = -y
+                y = temp
+            }
+
+            return Coordinate(x, y)
+        }
+        val (newCoord, _) = instructions.fold(WaypointState(Coordinate.origin, Coordinate(10, -1))) { currState, next ->
+            val (ship, waypoint) = currState
+            val (action, operand) = next
+
+            val waypointState = when (action) {
+                'L' -> currState.copy(waypointVector = rotate(waypoint, 360 - operand))
+                'R' -> currState.copy(waypointVector = rotate(waypoint, operand))
+                'F' -> currState.copy(shipCoordinate = ship + waypoint * operand)
+                'N' -> currState.copy(waypointVector = waypoint + directionToCoord(0) * operand)
+                'S' -> currState.copy(waypointVector = waypoint + directionToCoord(180) * operand)
+                'E' -> currState.copy(waypointVector = waypoint + directionToCoord(90) * operand)
+                'W' -> currState.copy(waypointVector = waypoint + directionToCoord(270) * operand)
+                else -> throw AssertionError("invalid $action")
+            }
+
+            println(waypointState)
+            waypointState
+
+        }
         return abs(newCoord.x) + abs(newCoord.y)
     }
 
@@ -62,5 +105,5 @@ fun main() {
         R90
         F11
     """.trimIndent().lines().map { Day12.parseInput(it) }
-    println(Day12.part1(Day12.input))
+    println(Day12.part2(Day12.input))
 }
