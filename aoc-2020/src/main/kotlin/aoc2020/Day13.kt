f2bd27dbf42766b82a7e67112513e9a0b3832098
Felix de Souza <fdesouza@palantir.com>
2020-12-13 05:53:01 +0000

    Day 13 Shitty part 2

--- aoc-2020/src/main/kotlin/aoc2020/Day13.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day13.kt
@@ -5,14 +5,15 @@ import common.readLines
 object Day13 {
     val input = readLines("/aoc2020/day13.txt").let { parseInput(it) }
 
-    fun parseInput(lines: List<String>): Pair<Int, List<Int>> {
+    fun parseInput(lines: List<String>): Pair<Int, List<Pair<Int, Int>>> {
         val (first, next) = lines
 
-        return first.toInt() to next.split(",").filter { it != "x" }.map { it.toInt() }
+        return first.toInt() to next.split(",").withIndex().filter { it.value != "x" }.map { (index, value) -> index to value.toInt() }
     }
 
-    fun part1(input: Pair<Int, List<Int>>): Long {
-        val (start, numbers) = input
+    fun part1(input: Pair<Int, List<Pair<Int, Int>>>): Long {
+        val (start, bla) = input
+        val numbers = bla.map { it.second }
 
         val (id, wait) = numbers.associateWith { -1 * ((start % it) - it) }
             .entries
@@ -22,13 +23,30 @@ object Day13 {
 
         return id*wait.toLong()
     }
+
+    fun part2(input: Pair<Int, List<Pair<Int, Int>>>): Long {
+        // chinese remainder theorem then plus/minus product until first answer
+
+        input.second
+            .map { (index, value) -> "x = ${value - index} mod $value " }
+            .forEach { println(it) }
+
+        return 0
+    }
+
+    fun solvePart2(input: Pair<Int, List<Pair<Int, Int>>>, solvedCrt: Long): Long {
+        val n = input.second.map { it.second }.map { it.toLong() }.reduce { a, b -> a * b}
+        return (solvedCrt % n + n) % n
+    }
 }
 
 fun main() {
     val testInput = """
         939
         7,13,x,x,59,x,31,19
-    """.trimIndent().lines().let { Day13.parseInput(it) }.let { println(Day13.part1(it)) }
+    """.trimIndent().lines().let { Day13.parseInput(it) }
 
     println(Day13.part1(Day13.input))
+    println(Day13.part2(Day13.input))
+    println(Day13.solvePart2(Day13.input, 267666633441334559))
 }
