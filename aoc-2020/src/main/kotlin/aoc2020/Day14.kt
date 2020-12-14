15c4a2a709775d43e4cbe01098948c88494b0d8e
Felix de Souza <fdesouza@palantir.com>
2020-12-14 05:52:00 +0000

    Day 14 part 2

--- aoc-2020/src/main/kotlin/aoc2020/Day14.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day14.kt
@@ -16,7 +16,6 @@ object Day14 {
                 line.startsWith("mask = ") -> {
                     val mask = line.slice(7 until line.length)
                     currentMask = mask.reversed().withIndex().filterNot { it.value == 'X' }.map { (index, value) -> index to value.toInt() - '0'.toInt() }.toMap()
-                    println("mask: $currentMask")
                 }
                 line.startsWith("mem") -> {
                     val address = line.slice(line.indexOf('[') + 1 until line.indexOf(']')).toInt()
@@ -25,12 +24,49 @@ object Day14 {
                     val ones = currentMask.filterValues { it == 1 }.map { 1L shl it.key }.fold(0L) { a, b -> a.or(b) }
                     val zeros = currentMask.filterValues { it == 0 }.map { 1L shl it.key }.fold(0L) {a, b -> a.or(b)}
 
-                    println(address)
-                    println(value)
-
                     val maskedValue = value.or(ones).and(zeros.inv())
-//                    println(Integer.toBinaryString(maskedValue))
                     map[address] = maskedValue
+                }
+            }
+        }
+
+        return map.values.sum()
+    }
+
+    fun part2(input: List<String>): Long {
+        val map: MutableMap<Long, Long> = mutableMapOf()
+        var currentMask: Map<Int, Int> = emptyMap()
+        var xs: List<Int> = emptyList()
+
+        for (line in input) {
+            when {
+                line.startsWith("mask = ") -> {
+                    val mask = line.slice(7 until line.length)
+                    currentMask = mask.reversed().withIndex().filterNot { it.value == 'X' }.map { (index, value) -> index to value.toInt() - '0'.toInt() }.toMap()
+                    xs = mask.reversed().withIndex().filter { it.value == 'X' }.map { it.index }.sorted()
+                }
+                line.startsWith("mem") -> {
+                    val address = line.slice(line.indexOf('[') + 1 until line.indexOf(']')).toLong()
+                    val value = line.slice(line.indexOf('=') + 2 until line.length).toLong()
+
+                    val ones = currentMask.filterValues { it == 1 }.map { 1L shl it.key }.fold(0L) { a, b -> a.or(b) }
+                    val maskedAddress = address.or(ones)
+
+                    if (xs.isEmpty()) {
+                        map[maskedAddress] = value
+                    } else {
+                        val combinations = Math.pow(2.toDouble(), xs.size.toDouble()).toLong()
+                        for (i in 0 until combinations) {
+                            val associations = xs.withIndex().associateWith { (index, _) -> (i and (1L shl index) shr index)}.mapKeys { it.key.value }
+                            val ones = associations.filterValues { it == 1L }.map { 1L shl it.key }.fold(0L) { a, b -> a.or(b) }
+                            val zeros = associations.filterValues { it == 0L }.map { 1L shl it.key }.fold(0L) {a, b -> a.or(b)}
+
+                            val finalMarkedAddress = maskedAddress.or(ones).and(zeros.inv())
+                            println("map[$finalMarkedAddress] = $value")
+                            map[finalMarkedAddress] = value
+                        }
+                    }
+//                    println(Integer.toBinaryString(maskedValue))
                 }
             }
         }
@@ -41,11 +77,13 @@ object Day14 {
 
 fun main() {
     val testInput = """
-        mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-        mem[8] = 11
-        mem[7] = 101
-        mem[8] = 0
+        mask = 000000000000000000000000000000X1001X
+        mem[42] = 100
+        mask = 00000000000000000000000000000000X0XX
+        mem[26] = 1
     """.trimIndent().lines()
 
     println(Day14.part1(Day14.input))
+
+    println(Day14.part2(Day14.input))
 }
