a2a6dd7099ccdb57265100d0dd4a526f99928831
Felix de Souza <fdesouza@palantir.com>
2020-12-18 05:55:03 +0000

    Day 18 part 2

--- aoc-2020/src/main/kotlin/aoc2020/Day18.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day18.kt
@@ -6,26 +6,26 @@ object Day18 {
 
     val input = openFile("/aoc2020/day18.txt").lines().map { it.replace(" ",  "") }
 
-    fun evaluate(line: String): Long {
+    fun findMatchingParenthesis(i: Int, line: String): Int {
+        var count = 1
+        var endIndex = i
+        while (count != 0) {
+            when (line[endIndex]) {
+                '(' -> count += 1
+                ')' -> count -= 1
+                else -> {}
+            }
+            endIndex += 1
+        }
+
+        return endIndex
+    }
+
+    fun evaluate(line: String, prec: Boolean = false): Long {
         var cur = 0L
         var index = 0
 
         var operation: ((a: Long, b: Long) -> Long)? = null
-
-        fun findMatchingParenthesis(i: Int): Int {
-            var count = 1
-            var endIndex = i
-            while (count != 0) {
-                when (line[endIndex]) {
-                    '(' -> count += 1
-                    ')' -> count -= 1
-                    else -> {}
-                }
-                endIndex += 1
-            }
-
-            return endIndex
-        }
 
         fun pushNum(num: Long) {
             if (operation == null) {
@@ -56,14 +56,18 @@ object Day18 {
                 '*' -> {
                     operation = { a, b -> a * b }
                     println("operation: *")
+                    if (prec) {
+                        pushNum(evaluate(line.slice(index + 1 until line.length), prec))
+                        return cur
+                    }
                     index += 1
                 }
                 '(' -> {
-                    val endParenthesis = findMatchingParenthesis(index + 1)
+                    val endParenthesis = findMatchingParenthesis(index + 1, line)
                     val inner = line.slice(index + 1 until endParenthesis - 1)
                     println("---")
                     println("inner: $inner")
-                    val parenthesisEvaluated = evaluate(inner)
+                    val parenthesisEvaluated = evaluate(inner, prec)
                     println("evaluated inner: $inner -> $parenthesisEvaluated")
                     println("---")
                     pushNum(parenthesisEvaluated)
@@ -77,10 +81,14 @@ object Day18 {
 
     fun part1(lines: List<String>): Long {
         return lines.map { evaluate(it) }.sum()
+    }
 
+    fun part2(lines: List<String>): Long {
+        return lines.map { evaluate(it, true) }.sum()
     }
 }
 
 fun main() {
     println(Day18.part1(Day18.input))
+    println(Day18.part2(Day18.input))
 }
