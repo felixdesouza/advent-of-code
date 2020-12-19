bc64300e26ecac918e0c6d1bd50a5f301cff15df
Felix de Souza <fdesouza@palantir.com>
2020-12-19 06:50:02 +0000

    Day 19 part 2

--- aoc-2020/src/main/kotlin/aoc2020/Day19.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day19.kt
@@ -3,6 +3,7 @@ package aoc2020
 import com.google.common.graph.GraphBuilder
 import com.google.common.graph.MutableGraph
 import common.openFile
+import common.repeat
 import java.util.regex.Pattern
 
 object Day19 {
@@ -45,7 +46,8 @@ object Day19 {
         }
 
         val definition = unresolved[n]!!
-        val nDeps = definition.replace("|", "").split("\\s+".toRegex()).map { it.toInt() }.distinct()
+        val nDeps = definition.replace("|", "").replace("+", "").split("\\s+".toRegex()).map { it.toInt() }.distinct()
+
         println("deps for $n: $nDeps")
         val (newResolved, newUnresolved) = nDeps.fold(resolved to unresolved) { (currResolved, currUnresolved), next ->
             resolve(next, currResolved, currUnresolved)
@@ -61,16 +63,27 @@ object Day19 {
         return newResolved.plus(n to resolvedDefinition) to newUnresolved.minus(n)
     }
 
-    fun part1(input: Input): Int {
+    fun part1(input: Input, n: Int = 0): Int {
         val (resolved, unresolved, thingsToCheck) = input
-        val (finalResolved, bla) = resolve(0, resolved, unresolved)
+        val (finalResolved, bla) = resolve(n, resolved, unresolved)
 
         if (bla.isNotEmpty()) throw AssertionError("missed something")
-        val rootResolution = finalResolved[0]!!
+        val rootResolution = finalResolved[n]!!
 
         val compile = Pattern.compile("^$rootResolution$")
 
         return thingsToCheck.count { compile.matcher(it).matches() }
+    }
+
+    fun part2(input: Input): Int {
+        val (_, unresolved, _) = input
+        val unrolled = (1..90).map {
+            val first = sequenceOf(42).repeat().take(it).joinToString(" ")
+            val second = sequenceOf(31).repeat().take(it).joinToString(" ")
+            "$first $second"
+        }.joinToString(" | ")
+
+        return part1(input.copy(unresolved = unresolved.plus(8 to "42+").plus(11 to unrolled)))
     }
 
 
@@ -78,21 +91,54 @@ object Day19 {
 
 fun main() {
     val testInput = """
-        0: 4 1 5
-        1: 2 3 | 3 2
-        2: 4 4 | 5 5
-        3: 4 5 | 5 4
-        4: "a"
-        5: "b"
+42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
 
-        ababbb
-        bababa
-        abbbab
-        aaabbb
-        aaaabbb
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
     """.trimIndent().let { Day19.parseInput(it) }
 
-//    println(testInput)
-
-    println(Day19.part1(Day19.input))
+    println(Day19.part2(Day19.input))
 }
