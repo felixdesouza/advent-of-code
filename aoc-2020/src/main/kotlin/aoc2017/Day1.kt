421eede0919ffa82b6f2235cc0f8725cf98b3417
Felix de Souza <fdesouza@palantir.com>
2020-12-20 20:29:23 +0000

    2017: Day 1 part 2

--- aoc-2020/src/main/kotlin/aoc2017/Day1.kt
+++ aoc-2020/src/main/kotlin/aoc2017/Day1.kt
@@ -6,12 +6,22 @@ object Day1 {
     val input = openFile("/aoc2017/day1.txt").toList()
 
     fun part1(input: List<Char>): Int {
-        val complete = input + input.first()
-        return complete.windowed(2).filter { (a, b) -> a == b }.map { it.first() }.map { it - '0' }.sum()
+        return compute(input, 1)
+    }
+
+    fun part2(input: List<Char>): Int {
+        return compute(input, input.size / 2)
+    }
+
+    private fun compute(input: List<Char>, step: Int): Int {
+        return input.withIndex().filter { (index, value) -> input[(index + step) % input.size] == value }
+                .map { it.value - '0' }
+                .sum()
     }
 
 }
 
 fun main() {
     println(Day1.part1(Day1.input))
+    println(Day1.part2(Day1.input))
 }
