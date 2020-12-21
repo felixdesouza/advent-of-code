594291b1c42111a3c1fc2fcb7c306646b18b0c2f
Felix de Souza <fdesouza@palantir.com>
2020-12-21 18:22:35 +0000

    2017: Day 6 part 1

---
+++ aoc-2020/src/main/kotlin/aoc2017/Day6.kt
@@ -0,0 +1,49 @@
+package aoc2017
+
+import common.openFile
+
+object Day6 {
+    val input = openFile("/aoc2017/day6.txt")
+            .split("\\s+".toRegex())
+            .map { it.toInt() }
+
+    private fun List<Int>.tick(): List<Int> {
+        val n = this.size
+        val (maxIndex, maxValue) = this.withIndex().maxBy { it.value }!!
+        println("max = ${maxIndex}, $maxValue")
+        val minJump = maxValue / n
+//        println("minJump = ${minJump}")
+        return this.withIndex().map { (index, value) ->
+            val offset = (((index - maxIndex - 1) % n) + n) % n
+            val extra = if (offset < maxValue % n) 1 else 0
+            val currentCompensate = if (index == maxIndex) -maxValue else 0
+//            println("---")
+//            println("index = ${index}")
+//            println("value = ${value}")
+//            println("offset = ${offset}")
+//            println("extra = ${extra}")
+//            println("currentCompensate = ${currentCompensate}")
+            value + minJump + extra + currentCompensate
+        }
+    }
+
+    fun part1(list: List<Int>): Int {
+
+        tailrec fun iterate(seen: Set<List<Int>>, steps: Int, current: List<Int>): Int {
+            val next = current.tick()
+            return if (next in seen) {
+                steps + 1
+            } else {
+                iterate(seen.plusElement(next), steps + 1, next)
+            }
+        }
+
+        return iterate(emptySet(), 0, list)
+
+    }
+}
+
+fun main() {
+    println(Day6.part1(Day6.input))
+}
+
