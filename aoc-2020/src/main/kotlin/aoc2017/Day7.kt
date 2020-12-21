cf583f96d0b8a7c762c139cc5196c785059b9d89
Felix de Souza <fdesouza@palantir.com>
2020-12-21 19:43:12 +0000

    2017 Day 7 part 2

--- aoc-2020/src/main/kotlin/aoc2017/Day7.kt
+++ aoc-2020/src/main/kotlin/aoc2017/Day7.kt
@@ -26,20 +26,63 @@ object Day7 {
 
     data class Program(val name: String, val weight: Int, val deps: List<String>)
     fun parseInput(line: String): Program {
-        println(line)
         val match = regex.find(line)!!
         val (name, weight, _, rawDeps) = match.destructured
 
-        return Program(name, weight.toInt(), rawDeps.split(", "))
+        return Program(name, weight.toInt(), rawDeps.split(", ").filter { it.isNotBlank() })
     }
 
     fun part1(input: List<Program>): String {
         val allDeps = input.flatMap { it.deps }.toSet()
         return input.find { it.name !in  allDeps }!!.name
     }
+
+    fun part2(input: List<Program>): Int {
+        val programsByName = input.associateBy { it.name }
+
+        fun compute(sums: Map<Program, Int>, curr: Program): Map<Program, Int> {
+            val updatedSums = curr.deps.fold(sums) { currentSums, dep ->
+                val program = programsByName[dep]
+                compute(currentSums, program!!)
+            }
+            println("updatedSums = ${updatedSums}")
+            return updatedSums.plus(curr to (curr.weight + curr.deps.sumBy { updatedSums[programsByName[it]!!]!! }))
+        }
+
+        val base = programsByName[part1(input)]!!
+        val sums = compute(emptyMap(), base).onEach { println(it) }
+
+        tailrec fun findDeepestUnbalanced(sums: Map<Program, Int>, curr: Program): Program {
+            // find deepest unbalanced
+            val childSums = curr.deps.associateWith { sums[programsByName[it]!!]!! }
+            val unbalancedChild = childSums.entries.groupBy({ it.value }, { it.key }).entries.firstOrNull { it.value.size == 1 }?.value?.firstOrNull()
+
+            return if (unbalancedChild == null) {
+                curr
+            } else {
+                findDeepestUnbalanced(sums, programsByName[unbalancedChild]!!)
+            }
+        }
+
+        fun ancestors(ancestors: Map<Program, Program>, curr: Program): Map<Program, Program> {
+            return curr.deps.map { programsByName[it]!! to curr }.toMap() + curr.deps.fold(ancestors) { curr, next -> ancestors(curr, programsByName[next]!!)}
+        }
+
+        val deepestUnbalanced = findDeepestUnbalanced(sums, base)
+        val ancestors = ancestors(emptyMap(), base)
+
+        ancestors.map { (k, v) -> k.name to v.name }.onEach { (a, b) -> println("$a -> $b") }
+
+        val parentOfUnbalanced = ancestors[deepestUnbalanced]!!
+        val sibling = parentOfUnbalanced.deps.first { it != deepestUnbalanced.name }.let { programsByName[it]!! }
+
+        return sums[sibling]!! - sums[deepestUnbalanced]!! + deepestUnbalanced.weight
+    }
 }
 
 fun main() {
     println(Day7.part1(Day7.input))
+    println(Day7.part2(Day7.input))
+
 
 }
