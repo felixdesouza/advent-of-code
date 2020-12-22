36a23913da45c6ea79bc44e6a105869bf1638da5
Felix de Souza <fdesouza@palantir.com>
2020-12-22 06:29:28 +0000

    Day 22 part 2 eurgh

--- aoc-2020/src/main/kotlin/aoc2020/Day22.kt
+++ aoc-2020/src/main/kotlin/aoc2020/Day22.kt
@@ -24,6 +24,17 @@ object Day22 {
     """.trimIndent()
             .let { parseInput(it) }
 
+    val testInput2 = """
+        Player 1:
+        43
+        19
+
+        Player 2:
+        2
+        29
+        14
+    """.trimIndent().let { parseInput(it) }
+
     fun parseInput(raw: String): Pair<Player, Player> {
         val (player1, player2) = raw.split("\n\n").map { Player.parsePlayer(it) }.let { LinkedList(it) }
         return player1 to player2
@@ -35,45 +46,80 @@ object Day22 {
                 return Player(raw.lines().drop(1).map { it.toInt() }.let { LinkedList(it) })
             }
         }
+
+        fun clone(n: Int = deck.size): Player {
+            if (deck.size < n) throw AssertionError("something is wrong")
+            return Player(LinkedList(deck.take(n)))
+        }
+        fun shouldSubgame(n: Int): Boolean = deck.size >= n
+
+        fun score(): Long = (deck.size downTo 1).zip(deck).map { (score, card) -> score * card.toLong() }.sum()
     }
 
-    fun Pair<Player, Player>.tick(): Pair<Player, Player> {
-        val (player1, player2) = this
-        if (player1.deck.isEmpty() || player2.deck.isEmpty()) {
-            throw AssertionError("game is over")
-        }
-
-        val p1 = player1.deck.removeFirst()
-        val p2 = player2.deck.removeFirst()
-
-        val sorted = listOf(p1, p2).sortedDescending()
-
-        if (p1 > p2) {
-            player1.deck.addAll(sorted)
-        } else {
-            player2.deck.addAll(sorted)
-        }
-
-        return player1 to player2
+    fun game(state: State): Player {
+        return state.winner() ?: game(state.tick())
     }
 
-    fun Pair<Player, Player>.winner(): Player? {
-        val (player1, player2) = this
-        val loser = listOf(player1, player2).firstOrNull { it.deck.isEmpty() }
+    data class State(val player1: Player, val player2: Player, val previousState: Set<Pair<Player, Player>>, val recursive: Boolean) {
+        fun winner(): Player? {
+            if (recursive && (player1 to player2) in previousState) {
+                return player1
+            }
+            val loser = listOf(player1, player2).firstOrNull { it.deck.isEmpty() }
+            return loser?.let { (listOf(player1, player2) - it).first() }
+        }
 
-        return loser?.let { (listOf(player1, player2) - it).first() }
+        fun tick(): State {
+            if (player1.deck.isEmpty() || player2.deck.isEmpty()) {
+                throw AssertionError("game is over")
+            }
+
+            val savedPlayer1 = player1.clone()
+            val savedPlayer2 = player2.clone()
+
+//            println("begin tick: player 1: $player1 player 2: $player2")
+
+            val p1 = player1.deck.removeFirst()
+            val p2 = player2.deck.removeFirst()
+
+            val winner = if (recursive && player1.shouldSubgame(p1) && player2.shouldSubgame(p2)) {
+                val newPlayer1 = player1.clone(p1)
+                val newPlayer2 = player2.clone(p2)
+
+                val winner = game(State(newPlayer1, newPlayer2, emptySet(), recursive))
+                if (winner == newPlayer1) player1 else player2
+            } else {
+                if (p1 > p2) player1 else player2
+            }
+
+            when (winner) {
+                player1 -> {
+                    player1.deck.add(p1)
+                    player1.deck.add(p2)
+                }
+                player2 -> {
+                    player2.deck.add(p2)
+                    player2.deck.add(p1)
+                }
+                else -> {
+                    throw AssertionError("unexpected winner")
+                }
+            }
+
+            return copy(previousState = previousState.plus(savedPlayer1 to savedPlayer2))
+        }
     }
 
     fun part1(state: Pair<Player, Player>): Long {
-        fun game(state: Pair<Player, Player>): Player {
-            return state.winner() ?: game(state.tick())
-        }
+        return game(State(state.first, state.second, emptySet(), false)).score()
+    }
 
-        val winner = game(state)
-        return (winner.deck.size downTo 1).zip(winner.deck).map { (score, card) -> score * card.toLong() }.sum()
+    fun part2(state: Pair<Player, Player>): Long {
+        return game(State(state.first, state.second, emptySet(), true)).score()
     }
 }
 
 fun main() {
-    println(Day22.part1(Day22.input))
+//    println(Day22.part1(Day22.input))
+    println(Day22.part2(Day22.input))
 }
