c9cc626088e15cd8c3a1750fd5f98f828ed2fd85
Felix de Souza <fdesouza@palantir.com>
2020-12-23 00:53:13 +0000

    2017 Day 11 part 2

--- aoc-2020/src/main/kotlin/aoc2017/Day11.kt
+++ aoc-2020/src/main/kotlin/aoc2017/Day11.kt
@@ -1,6 +1,7 @@
 package aoc2017
 
 import common.openFile
+import java.lang.Integer.max
 import java.lang.Integer.min
 
 object Day11 {
@@ -19,14 +20,26 @@ object Day11 {
             .shortcut("se", "n", "ne")
             .shortcut("sw", "n", "nw")
 
+    private fun Map<String, Int>.reduce() = generateSequence(this) { iterate() }.windowed(2).first { (a, b) -> a == b}.first()
+
+    private fun Map<String, Int>.steps() = values.sum()
+
     fun part1(input: List<String>): Int {
         val counts = input.groupingBy { it }.eachCount()
-        return generateSequence(counts) { counts.iterate() }.windowed(2).first { (a, b) -> a == b }.first().values.sum()
+        return counts.reduce().steps()
+    }
+
+    fun part2(input: List<String>): Int {
+        val (_, maxSteps) = input.fold(emptyMap<String, Int>() to 0) { (state, maxSteps), next ->
+            val newState = (state + (next to (state[next] ?: 0) + 1)).reduce()
+            newState to max(maxSteps, newState.steps())
+        }
+
+        return maxSteps
     }
 
     private fun Map<String, Int>.shortcut(direction1: String, direction2: String, resolved: String): Map<String, Int> {
         val resolvedAdditions = min(this[direction1] ?: 0, this[direction2] ?: 0)
-        println("direction1 = [${direction1}], direction2 = [${direction2}], resolved = [${resolved}], resolvedAdditions = $resolvedAdditions")
         return listOf(direction1, direction2).fold(this) { state, next ->
             state + (next to (state[next] ?: 0) - resolvedAdditions)
         }.let { it + (resolved to (it[resolved] ?: 0) + resolvedAdditions) }
@@ -42,4 +55,5 @@ object Day11 {
 
 fun main() {
     Day11.part1(Day11.input).also { println(it) }
+    Day11.part2(Day11.input).also { println(it) }
 }
