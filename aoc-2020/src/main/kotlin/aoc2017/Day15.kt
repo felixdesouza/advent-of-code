dd987081b741d957f25f46b22663c80c7db7b810
Felix de Souza <fdesouza@palantir.com>
2020-12-23 15:38:49 +0000

    2017: Day 15 part 2

--- aoc-2020/src/main/kotlin/aoc2017/Day15.kt
+++ aoc-2020/src/main/kotlin/aoc2017/Day15.kt
@@ -1,6 +1,6 @@
 package aoc2017
 
-import aoc2017.Day15.part1
+import aoc2017.Day15.part2
 
 object Day15 {
 
@@ -8,21 +8,25 @@ object Day15 {
     val testInput = 65 to 8921
     val factors = 16807L to 48271L
     val modulus = 2147483647
+    val mask = (1L shl 16) - 1
 
-    fun generate(starting: Long, factor: Long): Sequence<Long> {
-        return generateSequence(starting) { (it * factor) % modulus }
+    fun generate(starting: Long, factor: Long, multiplier: Int = 1): Sequence<Long> {
+        return generateSequence(starting) { (it * factor) % modulus }.drop(1).filter { it % multiplier == 0L }
     }
 
-    fun Pair<Int, Int>.part1(): Int {
-        val mask = (1L shl 16) - 1
+    private fun Long.lsb() = this.and(mask)
+
+    fun Pair<Int, Int>.part1(): Int = run(1 to 1, 40_000_000)
+    fun Pair<Int, Int>.part2(): Int = run(4 to 8, 5_000_000)
+
+    private fun Pair<Int, Int>.run(multiplier: Pair<Int, Int>, n: Int): Int {
         val (a, b) = this
-        fun Long.lsb() = this.and( mask)
-        return generate(a.toLong(), factors.first).zip(generate(b.toLong(), factors.second))
-                .take(40_000_000)
+        return generate(a.toLong(), factors.first, multiplier.first).zip(generate(b.toLong(), factors.second, multiplier.second))
+                .take(n)
                 .count { (lsbA, lsbB) -> lsbA.lsb() == lsbB.lsb() }
     }
 }
 
 fun main() {
-    println(Day15.input.part1())
+    println(Day15.input.part2())
 }
