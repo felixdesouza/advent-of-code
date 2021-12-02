c1cbb2b7b654ef9b478384e87a6a30621ced80af
Felix de Souza <fdesouza@palantir.com>
2021-12-02 05:30:32 +0000

    Tidy up

--- aoc-2020/src/main/kotlin/aoc2021/Day2.kt
+++ aoc-2020/src/main/kotlin/aoc2021/Day2.kt
@@ -4,51 +4,45 @@ import common.readLines
 
 object Day2 {
 
-    val input = readLines("/aoc2021/day2.txt")
+    fun parseInput(lines: List<String>) = lines
             .map { it.split(" ") }
             .map { (a, b) -> a to b.toInt() }
 
-    val testInput = listOf<String>(
-            "forward 5",
-            "down 5",
-            "forward 8",
-            "up 3",
-            "down 8",
-            "forward 2"
-    ).map { it.split(" ") }
-            .map { (a, b) -> a to b.toInt() }
+    val input = readLines("/aoc2021/day2.txt")
+            .let { parseInput(it) }
+
+    val testInput = listOf("forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2")
+            .let { parseInput(it) }
 
     fun part1(input: List<Pair<String, Int>>): Long {
         return input.fold(Pair(0, 0)) { position, next ->
             val (instruction, quantity) = next
-            val bla = if (instruction == "forward") {
-                position.copy(first = position.first + quantity)
-            } else if (instruction == "down") {
-                position.copy(second = position.second + quantity)
-            } else {
-                position.copy(second = position.second - quantity)
+            when (instruction) {
+                "forward" -> position.copy(first = position.first + quantity)
+                "down" -> position.copy(second = position.second + quantity)
+                "up" -> position.copy(second = position.second - quantity)
+                else -> throw AssertionError("unexpected")
             }
-            bla.also { println(it) }
         }.let { (pos, depth) -> pos.toLong() * depth }
     }
 
     fun part2(input: List<Pair<String, Int>>): Long {
         return input.fold(Triple(0, 0, 0)) { position, next ->
             val (instruction, quantity) = next
-            val bla = if (instruction == "forward") {
-                position.copy(first = position.first + quantity, second = position.second + quantity * position.third)
-            } else if (instruction == "down") {
-                position.copy(third = position.third + quantity)
-            } else {
-                position.copy(third = position.third - quantity)
+            val (horizontal, depth, aim) = position
+            when (instruction) {
+                "forward" -> position.copy(first = horizontal + quantity, second = depth + quantity * aim)
+                "down" -> position.copy(third = aim + quantity)
+                "up" -> position.copy(third = aim - quantity)
+                else -> throw AssertionError("unexpected")
             }
-            bla.also { println(it) }
         }.let { (pos, depth) -> pos.toLong() * depth }
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
         part1(input).also { println(it) }
+        part1(testInput).also { println(it) }
         part2(testInput).also { println(it) }
         part2(input).also { println(it) }
     }
