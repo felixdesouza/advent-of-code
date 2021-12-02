77978ce1295dc0ce1495af0b8a386d5ae8245717
Felix de Souza <fdesouza@palantir.com>
2021-12-02 05:14:47 +0000

    Day 2 part 1

---
+++ aoc-2020/src/main/kotlin/aoc2021/Day2.kt
@@ -0,0 +1,39 @@
+package aoc2021
+
+import common.readLines
+
+object Day2 {
+
+    val input = readLines("/aoc2021/day2.txt")
+            .map { it.split(" ") }
+            .map { (a, b) -> a to b.toInt() }
+
+    val testInput = listOf<String>(
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2"
+    ).map { it.split(" ") }
+            .map { (a, b) -> a to b.toInt() }
+
+    fun part1(input: List<Pair<String, Int>>): Long {
+        return input.fold(Pair(0, 0)) { position, next ->
+            val (instruction, quantity) = next
+            val bla = if (instruction == "forward") {
+                position.copy(first = position.first + quantity)
+            } else if (instruction == "down") {
+                position.copy(second = position.second + quantity)
+            } else {
+                position.copy(second = position.second - quantity)
+            }
+            bla.also { println(it) }
+        }.let { (pos, depth) -> pos.toLong() * depth }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        part1(input).also { println(it) }
+    }
+}
