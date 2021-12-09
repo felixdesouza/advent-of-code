2cb7ae75cedd90f5e2bde685e11a6087790e2c35
Felix de Souza <fdesouza@palantir.com>
2021-12-09 06:23:04 +0000

    Day 9

---
+++ aoc-2020/src/main/kotlin/aoc2021/Day9.kt
@@ -0,0 +1,76 @@
+package aoc2021
+
+import com.google.common.collect.Queues
+import common.Coordinate
+import common.Grid
+import common.openFile
+
+object Day9 {
+    fun parseInput(input: String): Grid<Int> = Grid.parse(input) { c -> c - '0'}
+
+    val input = openFile("/aoc2021/day9.txt")
+            .let { parseInput(it) }
+
+    val testInput = """
+        2199943210
+        3987894921
+        9856789892
+        8767896789
+        9899965678
+    """.trimIndent()
+            .let { parseInput(it) }
+
+    fun part1(input: Grid<Int>): Int {
+        return lowPoints(input).sumBy { input[it]!! + 1 }
+    }
+
+    // TODO: 09/12/2021 grid.filter and a grid.fold
+    private fun lowPoints(input: Grid<Int>): List<Coordinate> {
+        return (0 until input.numRows).fold(listOf()) { acc, row ->
+            (0 until input.numColumns).fold(acc) { colAcc, column ->
+                val coordinate = Coordinate(column, row)
+                val value = input[coordinate]!!
+                val isLowPoint = coordinate.neighbours()
+                        .map { input[it] ?: Integer.MAX_VALUE }
+                        .all { it > value }
+                if (isLowPoint) colAcc + coordinate else colAcc
+            }
+        }
+    }
+
+    private fun findBasinForLowPoint(input: Grid<Int>, coordinate: Coordinate): Map<Coordinate, Int> {
+        val queue = Queues.newArrayDeque<Coordinate>(setOf(coordinate))
+        val visited = mutableSetOf<Coordinate>()
+
+        while (queue.isNotEmpty()) {
+            val head = queue.pop()
+            if (head in visited) continue
+            visited.add(head)
+
+            head.neighbours()
+                    .filter { (input[it] ?: 9) < 9 }
+                    .filterNot { it in visited }
+                    .forEach { queue.add(it) }
+        }
+
+        return visited.associateWith { input[it]!! }
+    }
+
+    fun part2(input: Grid<Int>): Int {
+        val lowPoints = lowPoints(input)
+
+        return lowPoints.map { findBasinForLowPoint(input, it) }
+                .map { it.size }
+                .sortedDescending()
+                .take(3)
+                .fold(1) { acc, next -> acc * next }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(part1(testInput))
+        println(part1(input))
+        println(part2(testInput))
+        println(part2(input))
+    }
+}
