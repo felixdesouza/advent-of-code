ca65326bb71e690f796767a055bcdadbd27eeb14
Felix de Souza <fdesouza@palantir.com>
2021-12-18 12:41:45 +0000

    Day 18 tidy up.

--- aoc-2020/src/main/kotlin/aoc2021/Day18.kt
+++ aoc-2020/src/main/kotlin/aoc2021/Day18.kt
@@ -8,30 +8,6 @@ object Day18 {
     val input = openFile("/aoc2021/day18.txt").let { parse(it) }
 
     val testInput = """
-        [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-        [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-        [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-        [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
-        [7,[5,[[3,8],[1,4]]]]
-        [[2,[2,2]],[8,[8,1]]]
-        [2,9]
-        [1,[[[9,3],9],[[9,0],[0,7]]]]
-        [[[5,[7,4]],7],1]
-        [[[[4,2],2],6],[8,7]]
-    """.trimIndent()
-            .let { parse(it) }
-
-    val testInput3 = """
-        [1,1]
-        [2,2]
-        [3,3]
-        [4,4]
-        [5,5]
-        [6,6]
-    """.trimIndent()
-            .let { parse(it) }
-
-    val testInput2 = """
         [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
         [[[5,[2,8]],4],[5,[[9,9],0]]]
         [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
@@ -49,7 +25,6 @@ object Day18 {
 
     fun parseFish(input: String): SnailFishTree {
         fun helper(chars: List<Char>): Pair<SnailFishTree, List<Char>> {
-
             return when (val firstChar = chars[0]) {
                 in '0'..'9' -> {
                     Leaf((firstChar - '0').toLong()) to chars.subList(1, chars.size)
@@ -72,39 +47,37 @@ object Day18 {
             return explode(tree) ?: split(tree)
         }
 
-        return generateSequence(snailFishTree) { iterate(it) }
-                .withIndex()
-                .map { it.value }
-                .last()
+        return generateSequence(snailFishTree) { iterate(it) }.last()
     }
 
     fun explode(tree: SnailFishTree): SnailFishTree? {
         data class LeafBits(val long: Long, val left: Boolean)
+
         fun helper(t: SnailFishTree, depth: Int): Pair<SnailFishTree?, List<LeafBits>> {
-            return when (t) {
-                is Leaf -> null to emptyList()
+            when (t) {
+                is Leaf -> return null to emptyList()
                 is SnailFishNode -> {
                     if (depth >= 4) {
-                        Leaf(0) to listOf(
+                        return Leaf(0) to listOf(
                                 LeafBits((t.left as Leaf).value, true),
                                 LeafBits((t.right as Leaf).value, false))
-                    } else {
-                        val (newLeft, remainingBits) = helper(t.left, depth + 1)
-                        if (newLeft != null) {
-                            val (right, rest) = remainingBits.partition { !it.left }
-                            val newRight = if (right.isEmpty()) t.right else addToLeftMost(t.right, right.first().long)
-                            SnailFishNode(newLeft, newRight) to rest
-                        } else {
-                            val (newRight, remainingBits) = helper(t.right, depth + 1)
-                            if (newRight != null) {
-                                val (left, rest) = remainingBits.partition { it.left }
-                                val newLeft = if (left.isEmpty()) t.left else addToRightMost(t.left, left.first().long)
-                                SnailFishNode(newLeft, newRight) to rest
-                            } else {
-                                null to emptyList<LeafBits>()
-                            }
-                        }
                     }
+
+                    val (newLeft, remainingBitsFromLeft) = helper(t.left, depth + 1)
+                    if (newLeft != null) {
+                        val (right, rest) = remainingBitsFromLeft.partition { !it.left }
+                        val newRight = if (right.isEmpty()) t.right else addToLeftMost(t.right, right.first().long)
+                        return SnailFishNode(newLeft, newRight) to rest
+                    }
+
+                    val (newRight, remainingBitsFromRight) = helper(t.right, depth + 1)
+                    if (newRight != null) {
+                        val (left, rest) = remainingBitsFromRight.partition { it.left }
+                        val newLeft = if (left.isEmpty()) t.left else addToRightMost(t.left, left.first().long)
+                        return SnailFishNode(newLeft, newRight) to rest
+                    }
+
+                    return null to emptyList()
                 }
             }
         }
@@ -128,40 +101,26 @@ object Day18 {
     }
 
     fun split(tree: SnailFishTree): SnailFishTree? {
-        return when (tree) {
+        when (tree) {
             is Leaf -> {
-                if (tree.value >= 10) {
-                    SnailFishNode(Leaf(tree.value / 2), Leaf(tree.value / 2 + (tree.value % 2)))
-                } else {
-                    null
-                }
+                if (tree.value < 10) return null
+                return SnailFishNode(Leaf(tree.value / 2), Leaf(tree.value / 2 + (tree.value % 2)))
             }
-            is SnailFishNode -> {
-                val newLeft = split(tree.left)
-                if (newLeft != null) {
-                    SnailFishNode(newLeft, tree.right)
-                } else {
-                    val newRight = split(tree.right)
-                    if (newRight != null) {
-                        tree.copy(right = newRight)
-                    } else {
-                        null
-                    }
-                }
-            }
+            is SnailFishNode ->
+                return split(tree.left)?.let { SnailFishNode(it, tree.right) }
+                        ?: split(tree.right)?.let { tree.copy(right = it) }
         }
     }
 
     fun magnitude(tree: SnailFishTree): Long {
         return when (tree) {
             is Leaf -> tree.value
-            is SnailFishNode -> 3* magnitude(tree.left) + 2 * magnitude(tree.right)
+            is SnailFishNode -> 3 * magnitude(tree.left) + 2 * magnitude(tree.right)
         }
     }
 
     fun part1(input: List<SnailFishTree>): Long {
         val combined = input.reduce { acc, tree -> reduce(acc + tree) }
-        println(combined)
         return magnitude(combined)
     }
 
@@ -185,15 +144,12 @@ sealed class SnailFishTree {
     operator fun plus(other: SnailFishTree): SnailFishTree = SnailFishNode(this, other)
     override fun toString(): String = when (this) {
         is Leaf -> "${this.value}"
-        is SnailFishNode -> {
-            val left = left.toString()
-            val right = right.toString()
-            "[$left,$right]"
-        }
+        is SnailFishNode -> "[$left,$right]"
     }
 }
 
 data class SnailFishNode(val left: SnailFishTree, val right: SnailFishTree) : SnailFishTree() {
     override fun toString() = super.toString()
 }
+
 data class Leaf(val value: Long) : SnailFishTree()
